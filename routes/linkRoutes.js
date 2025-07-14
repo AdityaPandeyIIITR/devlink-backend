296c4ad5dd1d6eafe4522b4113c16e972165e14c
AdityaPandeyIIITR <129857967+AdityaPandeyIIITR@users.noreply.github.com>
2025-07-14 16:09:41 +0530

    Add handleRedirect import from clickController

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createShortLink } = require("../controllers/linkController");
 const authenticateUser = require("../middlewares/authMiddleware");
+const { handleRedirect } = require("../controllers/clickController");
 
 router.post("/shorten", authenticateUser, createShortLink);
 module.exports = router;
